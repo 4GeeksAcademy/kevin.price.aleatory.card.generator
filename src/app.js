37a3808d72e4302033454158a4ac1324fd0baa5f
Kevin Martín Price <k.pricevisuals@gmail.com>
2026-04-22 09:59:05 +0000

    Fist Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ const suits = [
+      { symbol: "♥", color: "red"   },
+      { symbol: "♦", color: "red"   },
+      { symbol: "♠", color: "black" },
+      { symbol: "♣", color: "black" }
+    ];
+ 
+    const values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+ 
+    function generateCard() {
+      var suit  = suits [Math.floor(Math.random() * 4)];
+      var value = values[Math.floor(Math.random() * 13)];
+ 
+      document.getElementById("center").textContent      = value;
+      document.getElementById("center").className        = "center " + suit.color;
+ 
+      document.getElementById("corner-top").innerHTML    = value + "<br>" + suit.symbol;
+      document.getElementById("corner-top").className    = "corner-top " + suit.color;
+ 
+      document.getElementById("corner-bottom").innerHTML = value + "<br>" + suit.symbol;
+      document.getElementById("corner-bottom").className = "corner-bottom " + suit.color;
+    }
+ 
+    window.onload = generateCard;
